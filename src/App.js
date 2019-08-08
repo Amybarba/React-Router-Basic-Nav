b4ae46d5cb5909205d5b662d4601c852721f4281
Amy Barba <Amybarba70@gmail.com>
2019-08-08 00:26:12 -0700

    project complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
 
 const App = () => (
-  <div>
-    <Navigation />
-  </div>
+<div className='App'>
+<Route exact path='/' component={Home}/>
+<Route path='/contact' component={Contact}/>
+<Route path='/about' component={About}/>
+<Navigation />
+</div>
 );
 
 export default App;
